7d883ad2bc1298407a0ae0feaf7683839dd8ac8e
Vishal Sanserwal <vsanse@outlook.com>
2022-10-15 13:42:32 +0530

    fix: :bug: Added drag and fixed css here and there

--- src/background.js
+++ src/background.js
@@ -1,56 +1,44 @@
-console.log("background.js");
-
-const webhookTestUrl =
-    "https://webhook.site/0d97f3f9-6a22-4e66-abfd-37637d9bae84";
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("SERVICE WORKER✅");
-    if (request.message === "start") {
-        embeddedRequestBox();
-        console.log("[turbo] embedded network request tab");
-    } else if (request.message === "requestDetails") {
-        console.log("[turbo] requestDetails", request.data);
-    } else if (request.message === "requestDetailsFromBackground") {
-        console.log("[turbo] requestDetailsFromBackground", request.data);
-    }
+  console.log("SERVICE WORKER✅");
+  if (request.message === "start") {
+    console.log("[turbo] embedded network request tab");
+  } else if (request.message === "requestDetails") {
+    console.log("[turbo] requestDetails", request.data);
+  } else if (request.message === "requestDetailsFromBackground") {
+    console.log("[turbo] requestDetailsFromBackground", request.data);
+  }
 });
 
 function startSendingRequests() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        currentTab = tabs[0];
-        // chrome.webRequest.onBeforeRequest.addListener(
-        //     function (details) {
-        //         console.log("onBeforeRequest", details);
-        //     },
-        //     { urls: ["*://*/*"] },
-        //     []
-        // );
-        chrome.webRequest.onCompleted.addListener(
-            function (details) {
-                chrome.tabs.sendMessage(currentTab.id, {
-                    message: "requestDetailsFromBackground",
-                    data: JSON.stringify(details),
-                });
-                chrome.tabs.query(
-                    { active: true, currentWindow: true },
-                    function (tabs) {
-                        chrome.tabs.sendMessage(
-                            tabs[0].id,
-                            {
-                                message: "requestDetailsFromBackground",
-                                data: JSON.stringify(details),
-                            },
-                            function (response) {}
-                        );
-                    }
-                );
-                fetch(webhookTestUrl + "?q=" + JSON.stringify(details));
-                console.log("sent onCompleted details");
-            },
-            { urls: ["*://*/*"] },
-            []
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    currentTab = tabs[0];
+    chrome.webRequest.onCompleted.addListener(
+      function (details) {
+        chrome.tabs.sendMessage(currentTab.id, {
+          message: "requestDetailsFromBackground",
+          data: JSON.stringify(details),
+        });
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            if (tabs[0]) {
+              chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                  message: "requestDetailsFromBackground",
+                  data: JSON.stringify(details),
+                },
+                function (response) {}
+              );
+            }
+          }
         );
-        console.log("events added");
-    });
+        console.log("sent onCompleted details");
+      },
+      { urls: ["*://*/*"] },
+      []
+    );
+    console.log("events added");
+  });
 }
 startSendingRequests();
--- src/run.js
+++ src/run.js
@@ -1,36 +1,29 @@
-console.log("[turbo] RUN.JS", chrome);
-
-// console.log("run.js", chrome.devtools.network);
-
 let REQUESTS = [];
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("[run.js] request", request.message);
-    if (request.message === "start") {
-        embeddedRequestBox();
-        // console.log("[turbo] embedded network request tab");
-    } else if (request.message === "requestDetails") {
-        // console.log("[turbo] requestDetails", request.data);
-    } else if (request.message === "requestDetailsFromBackground") {
-        // console.log("[turbo] requestDetailsFromBackground", request.data);
-        const details = JSON.parse(request.data);
-        const currentUrl = window.location.href;
-        const requestInitiator = details.initiator;
-        if (currentUrl.includes(requestInitiator)) {
-            REQUESTS.push(JSON.parse(request.data));
-            addRequestsInList();
-        }
+  console.log("[run.js] request", request.message);
+  if (request.message === "start") {
+    embeddedRequestBox();
+  } else if (request.message === "requestDetails") {
+  } else if (request.message === "requestDetailsFromBackground") {
+    const details = JSON.parse(request.data);
+    const currentUrl = window.location.href;
+    const requestInitiator = details.initiator;
+    if (currentUrl.includes(requestInitiator)) {
+      REQUESTS.push(JSON.parse(request.data));
+      addRequestsInList();
     }
+  }
 });
 
 function start() {
-    alert("started");
+  alert("started");
 }
 
 function embeddedRequestBox() {
-    const networkRequestsBox = document.createElement("div");
-    networkRequestsBox.id = "network-requests-box";
-    networkRequestsBox.innerHTML = `
+  const networkRequestsBox = document.createElement("div");
+  networkRequestsBox.id = "network-requests-box";
+  networkRequestsBox.innerHTML = `
         <div class="box-controllers">
             <div class="close">❌</div>
             <div class="minimize">➖</div>
@@ -38,53 +31,42 @@ function embeddedRequestBox() {
         <div class="requests-list">
         </div>
     `;
-    document.body.appendChild(networkRequestsBox);
-    const requestsList = networkRequestsBox.querySelector(".requests-list");
-    // REQUESTS.forEach((request) => {
-    //     const singleRequestWrapper = document.createElement("div");
-    //     singleRequestWrapper.classList.add("single-request-wrapper");
-    //     singleRequestWrapper.innerHTML = `
-    //         <div class="request-info">
-    //             <div class="request-method">${request.method}</div>
-    //             <div class="request-url">${request.url}</div>
-    //         </div>
-    //         <div class="request-status">${request.statusCode}</div>
-    //     `;
-    //     requestsList.appendChild(singleRequestWrapper);
-    // });
+  document.body.appendChild(networkRequestsBox);
 
-    const closeBtn = networkRequestsBox.querySelector(".close");
-    closeBtn.addEventListener("click", () => {
-        networkRequestsBox.remove();
-    });
+  dragElement(networkRequestsBox);
 
-    const minimizeBtn = networkRequestsBox.querySelector(".minimize");
-    minimizeBtn.addEventListener("click", () => {
-        networkRequestsBox.classList.toggle("minimized");
-    });
+  const closeBtn = networkRequestsBox.querySelector(".close");
+  closeBtn.addEventListener("click", () => {
+    networkRequestsBox.remove();
+  });
+
+  const minimizeBtn = networkRequestsBox.querySelector(".minimize");
+  minimizeBtn.addEventListener("click", () => {
+    networkRequestsBox.classList.toggle("minimized");
+  });
 }
 
 function addRequestsInList() {
-    const requestsList = document.querySelector(".requests-list");
-    if (!requestsList) {
-        console.log("no requests list");
-        return;
-    }
-    requestsList.innerHTML = "";
-    REQUESTS.forEach((request) => {
-        const singleRequestWrapper = document.createElement("div");
-        singleRequestWrapper.classList.add("single-request-wrapper");
-        singleRequestWrapper.innerHTML = `
+  const requestsList = document.querySelector(".requests-list");
+  if (!requestsList) {
+    console.log("no requests list");
+    return;
+  }
+  requestsList.innerHTML = "";
+  REQUESTS.forEach((request) => {
+    const singleRequestWrapper = document.createElement("div");
+    singleRequestWrapper.classList.add("single-request-wrapper");
+    singleRequestWrapper.innerHTML = `
             <div class="request-info">
                 <div class="request-method">${request.method}</div>
                 <div class="request-url">${request.url}</div>
             </div>
             <div class="request-status">${request.statusCode}</div>
         `;
-        requestsList.appendChild(singleRequestWrapper);
-    });
+    requestsList.appendChild(singleRequestWrapper);
+  });
 }
 
 window.onload = function () {
-    embeddedRequestBox();
+  embeddedRequestBox();
 };
