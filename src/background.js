0a7c97c95ec733fdd11e2a936635f124a247aa35
Neha Sanserwal <neha16sanserwal@gmail.com>
2022-10-15 22:36:30 +0530

    (feat) ✨wip

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+require('./hot-reload');
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log('SERVICE WORKER✅');
   if (request.message === 'start') {
     embeddedRequestBox();
@@ -12,31 +12,31 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function startSendingRequests() {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     const currentTab = tabs[0];
     chrome.webRequest.onCompleted.addListener(
-        function(details) {
-          chrome.tabs.sendMessage(currentTab.id, {
-            message: 'requestDetailsFromBackground',
-            data: JSON.stringify(details),
-          });
-          chrome.tabs.query(
-              {active: true, currentWindow: true},
-              function(tabs) {
-                chrome.tabs.sendMessage(
-                    tabs[0].id,
-                    {
-                      message: 'requestDetailsFromBackground',
-                      data: JSON.stringify(details),
-                    },
-                    function(response) {},
-                );
+      function (details) {
+        chrome.tabs.sendMessage(currentTab.id, {
+          message: 'requestDetailsFromBackground',
+          data: JSON.stringify(details),
+        });
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(
+              tabs[0].id,
+              {
+                message: 'requestDetailsFromBackground',
+                data: JSON.stringify(details),
               },
-          );
-          console.log('sent onCompleted details');
-        },
-        {urls: ['*://*/*']},
-        [],
+              function (response) { },
+            );
+          },
+        );
+        console.log('sent onCompleted details');
+      },
+      { urls: ['*://*/*'] },
+      [],
     );
     console.log('events added');
   });
